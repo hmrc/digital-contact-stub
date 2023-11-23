22c01026aa75a0bed7cc87f5acd5304d442aad61
Kapil Sanghvi <22366606+kjainsc@users.noreply.github.com>
2023-11-23 12:36:45 +0000

    DC-5754 Update digital-contact-stub to use HmrcStandardPage

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -18,13 +18,13 @@ import sbt._
 
 object AppDependencies {
 
-  private val bootstrapVersion = "7.13.0"
-  private val hmrcMongoVersion = "0.74.0"
+  private val bootstrapVersion = "8.0.0"
+  private val hmrcMongoVersion = "1.5.0"
 
   val compile = Seq(
     "uk.gov.hmrc"             %% "bootstrap-frontend-play-28" % bootstrapVersion,
     "uk.gov.hmrc"             %% "bootstrap-backend-play-28"  % bootstrapVersion,
-    "uk.gov.hmrc"             %% "play-frontend-hmrc"         % "6.4.0-play-28",
+    "uk.gov.hmrc"             %% "play-frontend-hmrc"         % "7.29.0-play-28",
     "com.beachape"            %% "enumeratum-play"            % "1.5.17",
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-28"         % hmrcMongoVersion
   )
