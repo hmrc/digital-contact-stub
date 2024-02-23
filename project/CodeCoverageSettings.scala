4f4292a4b556e5fa092a6792eab7a9730ea25ae7
Murali Raju <17339891+muralishub@users.noreply.github.com>
2024-02-23 14:58:24 +0000

    DC-5897: imi support list of suppression list

--- project/CodeCoverageSettings.scala
+++ project/CodeCoverageSettings.scala
@@ -16,7 +16,7 @@ object CodeCoverageSettings {
 
   val settings: Seq[Setting[_]] = Seq(
     ScoverageKeys.coverageExcludedPackages := excludedPackages.mkString(";"),
-    ScoverageKeys.coverageMinimumStmtTotal := 14.98,
+    ScoverageKeys.coverageMinimumStmtTotal := 14.00,
     ScoverageKeys.coverageFailOnMinimum := true,
     ScoverageKeys.coverageHighlighting := true
   )
