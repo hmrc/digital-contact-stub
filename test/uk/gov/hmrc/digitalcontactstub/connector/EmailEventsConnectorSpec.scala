c8765b9dc1df3af03008598dd14ba29fc959ee7c
Murali Raju <17339891+muralishub@users.noreply.github.com>
2024-02-28 16:50:48 +0000

    DC-5897: imi support list of suppression list

--- test/uk/gov/hmrc/digitalcontactstub/connector/EmailEventsConnectorSpec.scala
+++ test/uk/gov/hmrc/digitalcontactstub/connector/EmailEventsConnectorSpec.scala
@@ -21,16 +21,50 @@ import org.mockito.Mockito.when
 import org.mockito.MockitoSugar.mock
 import org.scalatest.concurrent.ScalaFutures.convertScalaFuture
 import org.scalatestplus.play.PlaySpec
-import play.api.libs.json.Writes
+import org.scalatestplus.play.guice.GuiceOneAppPerTest
+import play.api.libs.json.{JsValue, Json, Writes}
+import play.api.libs.ws.WSClient
 import uk.gov.hmrc.http.{HttpClient, HttpResponse}
 import uk.gov.hmrc.play.bootstrap.config.ServicesConfig
+
 import java.util.UUID
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.duration.DurationInt
+import scala.concurrent.{Await, ExecutionContext, Future}
 
-class EmailEventsConnectorSpec extends PlaySpec {
+class EmailEventsConnectorSpec extends PlaySpec with GuiceOneAppPerTest {
   "markSent" must {
     "return transId" in new Setup {
+
+
+      lazy val wsClient: WSClient = app.injector.instanceOf[WSClient]
+
+      def send(id: Int): Future[Int] = {
+        Thread.sleep(50)
+        val body: JsValue = Json.parse ((s"""{
+                                            |  "channel": "email",
+                                            |  "consent": true,
+                                            |  "address": "test$id@gmail.com",
+                                            |  "reason": "auto"
+                                            |}""".stripMargin))
+
+        val result = wsClient.url("https://contactpolicy.imiconnect.eu/v1/groups/yk7_hM8eQQeFwA3zHyfRgg/members").withHttpHeaders(("authorization", "Bearer XXX"), ("content-type", "application/json")).post(body)
+
+        result.map(x => x.status)
+
+      }
+      val ids = (1 to 20000).toList
+
+
+      val finalResult = Future.traverse(ids)(send)
+
+
+      Await.result(finalResult, 20.minutes)
+
+
+
+
+
       val connector = new EmailEventsConnector(servicesConfig, httpClient)
       val result = connector.markSent(UUID.randomUUID().toString).futureValue
       result mustBe transId
