6ac67bdb84daf5400232ee0f9d0b0762268501ff
Vineet Tyagi <22053543+tyagikumarvineet@users.noreply.github.com>
2026-03-19 14:42:51 +0000

    [DC-8625] - Update library versions to fix the vulnerabilities reported by Platform team (#48)
    * [DC-8625] Update library versions to fix the vulnerabilities wit IDs CVE-2025-66566 and CVE-2025-12183
    
    * [DC-8625] Update play-frontend-hmrc-play version

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -18,13 +18,13 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "10.3.0"
-  private val hmrcMongoVersion = "2.10.0"
+  private val bootstrapVersion = "10.7.0"
+  private val hmrcMongoVersion = "2.12.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30" % bootstrapVersion,
     "uk.gov.hmrc"       %% "bootstrap-backend-play-30"  % bootstrapVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "12.17.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "12.32.0",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"         % hmrcMongoVersion,
     "uk.gov.hmrc"       %% "domain-play-30"             % "13.0.0"
   )
