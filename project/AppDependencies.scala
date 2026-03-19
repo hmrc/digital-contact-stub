fc209e71590205481f999221f5c1928184fafefc
tyagikumarvineet <22053543+tyagikumarvineet@users.noreply.github.com>
2026-03-19 13:06:16 +0000

    [DC-8625] Update play-frontend-hmrc-play version

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -24,7 +24,7 @@ object AppDependencies {
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30" % bootstrapVersion,
     "uk.gov.hmrc"       %% "bootstrap-backend-play-30"  % bootstrapVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "12.17.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "12.32.0",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"         % hmrcMongoVersion,
     "uk.gov.hmrc"       %% "domain-play-30"             % "13.0.0"
   )
